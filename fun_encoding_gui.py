8af5d8c5a3f0dd3e1923a68d3cbcd4e4c6521de4
kigesui <kigesui@hotmail.com>
2016-04-27 15:12:20 -0400

    added a clear button

--- fun_encoding_gui.py
+++ fun_encoding_gui.py
@@ -15,13 +15,21 @@ class Application(Frame):
 
     def create_widgets(self):
         """ Create widgets """
+
+        # init variables
+        num_cols = 3
+
+        # create objects
+        # row 0
         Label(self,
-              text="Enter string to encode/decode"
-              ).grid(row=0, column=0, columnspan=2, sticky=W+E)
+              text="Enter string to encode or decode"
+              ).grid(row=0, column=0, columnspan=num_cols, sticky=W+E)
 
+        # row 1
         self.entry = Entry(self)
-        self.entry.grid(row=1, column=0, columnspan=2, sticky=W+E)
+        self.entry.grid(row=1, column=0, columnspan=num_cols, sticky=W+E)
 
+        # row 2
         Button(self,
                text="Encode",
                command=lambda: self.call_module('e')
@@ -30,9 +38,14 @@ class Application(Frame):
                text="Decode",
                command=lambda: self.call_module('d')
                ).grid(row=2, column=1, sticky=W+E)
+        Button(self,
+               text="Clear",
+               command=self.call_clear
+               ).grid(row=2, column=2, sticky=W+E)
 
+        # row 3
         self.result = Text(self, width=40, height=5, wrap=WORD)
-        self.result.grid(row=3, column=0, columnspan=2)
+        self.result.grid(row=3, column=0, columnspan=num_cols)
         # disable user input
         self.result.config(state=DISABLED)
         # enable selecting
@@ -55,8 +68,14 @@ class Application(Frame):
             self.result.unbind("<1>")
 
         self.result.config(state=NORMAL)  # enable change
-        self.result.delete(0.0, END)
-        self.result.insert(0.0, result)
+        self.result.delete(0.0, END)  # clear Text
+        self.result.insert(0.0, result)  # add result
+        self.result.config(state=DISABLED)  # disable change
+
+    def call_clear(self):
+        self.entry.delete(0, END)  # clear Entry
+        self.result.config(state=NORMAL)  # enable change
+        self.result.delete(0.0, END)  # clear Text
         self.result.config(state=DISABLED)  # disable change
 
 
